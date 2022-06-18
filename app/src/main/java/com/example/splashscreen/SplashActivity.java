b88446b9ec6ae858eed4dfc3f256aa8924887862
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-06-18 22:10:31 +0600

    Splash Screen added

---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent iHome = new Intent(SplashActivity.this, MainActivity.class);
+
+//        creating thread for asynchronous
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(iHome);
+                finish();
+            }
+        },5000);
+
+
+    }
+}
